ca0efd4588e9bcc11dc0ff15e7a37fbd7498d5d7
Flavius12 <info@evolsoft.tk>
2015-04-15 16:10:54 +0200

    Update Commands.php

--- src/CustomAlerts/Commands/Commands.php
+++ src/CustomAlerts/Commands/Commands.php
@@ -1,11 +1,11 @@
 <?php
 
 /*
- * CustomAlerts (v1.1) by EvolSoft
+ * CustomAlerts (v1.2) by EvolSoft
  * Developer: EvolSoft (Flavius12)
  * Website: http://www.evolsoft.tk
- * Date: 29/12/2014 09:28 AM (UTC)
- * Copyright & License: (C) 2014 EvolSoft
+ * Date: 14/04/2015 11:28 AM (UTC)
+ * Copyright & License: (C) 2014-2015 EvolSoft
  * Licensed under MIT (https://github.com/EvolSoft/CustomAlerts/blob/master/LICENSE)
  */
 
@@ -14,16 +14,14 @@ namespace CustomAlerts\Commands;
 use pocketmine\command\Command;
 use pocketmine\command\CommandExecutor;
 use pocketmine\command\CommandSender;
-use pocketmine\permission\Permission;
 use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
-use pocketmine\utils\TextFormat;
 
-use CustomAlerts\Main;
+use CustomAlerts\CustomAlertsAPI;
 
-class Commands extends PluginBase implements CommandExecutor{
+class Commands extends PluginBase implements CommandExecutor {
 
-	public function __construct(Main $plugin){
+	public function __construct(CustomAlertsAPI $plugin){
         $this->plugin = $plugin;
     }
     
@@ -35,19 +33,36 @@ class Commands extends PluginBase implements CommandExecutor{
     				$args[0] = strtolower($args[0]);
     				if($args[0]=="help"){
     					if($sender->hasPermission("customalerts.help")){
-    						$sender->sendMessage($this->plugin->translateColors("&", "&b-- &aAvailable Commands &b--"));
-    						$sender->sendMessage($this->plugin->translateColors("&", "&d/calerts info &b-&a Show info about this plugin"));
+    					    $sender->sendMessage($this->plugin->translateColors("&", "&b-- &aAvailable Commands &b--"));
+    						$sender->sendMessage($this->plugin->translateColors("&", "&d/calerts extensions &b-&a Show CustomAlerts loaded extensions"));
     						$sender->sendMessage($this->plugin->translateColors("&", "&d/calerts help &b-&a Show help about this plugin"));
+    						$sender->sendMessage($this->plugin->translateColors("&", "&d/calerts info &b-&a Show info about this plugin"));
     						$sender->sendMessage($this->plugin->translateColors("&", "&d/calerts reload &b-&a Reload the config"));
     						break;
     					}else{
     						$sender->sendMessage($this->plugin->translateColors("&", "&cYou don't have permissions to use this command"));
     						break;
     					}
+    				}elseif($args[0]=="extensions"){
+    					if($sender->hasPermission("customalerts.extensions")){
+    						$message = "";
+    						for($i = 0; $i < count(CustomAlertsAPI::getAPI()->getAllExtensions()); $i++){
+    							$extension = CustomAlertsAPI::getAPI()->getAllExtensions()[$i];
+    							$message .= "&a" . $extension->getFullName();
+    							if($i != count(CustomAlertsAPI::getAPI()->getAllExtensions()) - 1){
+    								$message .= "&f, ";
+    							}
+    						}
+    						$sender->sendMessage($this->plugin->translateColors("&", CustomAlertsAPI::PREFIX . "&fCustomAlerts loaded extensions (" . count(CustomAlertsAPI::getAPI()->getAllExtensions()) . "): " . $message));
+    				        break;
+    					}else{
+    						$sender->sendMessage($this->plugin->translateColors("&", "&cYou don't have permissions to use this command"));
+    						break;
+    					}
     				}elseif($args[0]=="info"){
     					if($sender->hasPermission("customalerts.info")){
-    						$sender->sendMessage($this->plugin->translateColors("&", Main::PREFIX . "&aCustomAlerts &dv" . Main::VERSION . " &adeveloped by&d " . Main::PRODUCER));
-    						$sender->sendMessage($this->plugin->translateColors("&", Main::PREFIX . "&aWebsite &d" . Main::MAIN_WEBSITE));
+    						$sender->sendMessage($this->plugin->translateColors("&", CustomAlertsAPI::PREFIX . "&aCustomAlerts &dv" . CustomAlertsAPI::VERSION . " &adeveloped by&d " . CustomAlertsAPI::PRODUCER));
+    						$sender->sendMessage($this->plugin->translateColors("&", CustomAlertsAPI::PREFIX . "&aWebsite &d" . CustomAlertsAPI::MAIN_WEBSITE));
     				        break;
     					}else{
     						$sender->sendMessage($this->plugin->translateColors("&", "&cYou don't have permissions to use this command"));
@@ -56,7 +71,7 @@ class Commands extends PluginBase implements CommandExecutor{
     				}elseif($args[0]=="reload"){
     					if($sender->hasPermission("customalerts.reload")){
     						$this->plugin->reloadConfig();
-    						$sender->sendMessage($this->plugin->translateColors("&", Main::PREFIX . "&aConfiguration Reloaded."));
+    						$sender->sendMessage($this->plugin->translateColors("&", CustomAlertsAPI::PREFIX . "&aConfiguration Reloaded."));
     				        break;
     					}else{
     						$sender->sendMessage($this->plugin->translateColors("&", "&cYou don't have permissions to use this command"));
@@ -64,7 +79,7 @@ class Commands extends PluginBase implements CommandExecutor{
     					}
     				}else{
     					if($sender->hasPermission("customalerts")){
-    						$sender->sendMessage($this->plugin->translateColors("&",  Main::PREFIX . "&cSubcommand &a" . $args[0] . " &cnot found. Use &a/calerts help &cto show available commands"));
+    						$sender->sendMessage($this->plugin->translateColors("&",  CustomAlertsAPI::PREFIX . "&cSubcommand &a" . $args[0] . " &cnot found. Use &a/calerts help &cto show available commands"));
     						break;
     					}else{
     						$sender->sendMessage($this->plugin->translateColors("&", "&cYou don't have permissions to use this command"));
@@ -74,8 +89,9 @@ class Commands extends PluginBase implements CommandExecutor{
     				}else{
     					if($sender->hasPermission("customalerts.help")){
     						$sender->sendMessage($this->plugin->translateColors("&", "&b-- &aAvailable Commands &b--"));
-    						$sender->sendMessage($this->plugin->translateColors("&", "&d/calerts info &b-&a Show info about this plugin"));
+    						$sender->sendMessage($this->plugin->translateColors("&", "&d/calerts extensions &b-&a Show CustomAlerts loaded extensions"));
     						$sender->sendMessage($this->plugin->translateColors("&", "&d/calerts help &b-&a Show help about this plugin"));
+    						$sender->sendMessage($this->plugin->translateColors("&", "&d/calerts info &b-&a Show info about this plugin"));
     						$sender->sendMessage($this->plugin->translateColors("&", "&d/calerts reload &b-&a Reload the config"));
     						break;
     					}else{
