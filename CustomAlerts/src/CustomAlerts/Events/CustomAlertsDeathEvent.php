2249ec99e147dbd8beaa1d10f3f9866a9239c324
Flavius12 <info@evolsoft.tk>
2015-07-14 15:30:30 +0200

    Update CustomAlertsDeathEvent.php

--- CustomAlerts/src/CustomAlerts/Events/CustomAlertsDeathEvent.php
+++ CustomAlerts/src/CustomAlerts/Events/CustomAlertsDeathEvent.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * CustomAlerts (v1.5) by EvolSoft
+ * CustomAlerts (v1.6) by EvolSoft
  * Developer: EvolSoft (Flavius12)
  * Website: http://www.evolsoft.tk
  * Date: 09/05/2015 01:12 PM (UTC)
