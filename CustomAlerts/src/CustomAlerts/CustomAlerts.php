b5e4417fad495e5e9b1402895d9a4c34801071b5
KanekiLeChomeur <52374013+KanekiLeChomeur@users.noreply.github.com>
2022-06-11 23:01:42 +0200

    Patch for Poggit's PHPStan

--- CustomAlerts/src/CustomAlerts/CustomAlerts.php
+++ CustomAlerts/src/CustomAlerts/CustomAlerts.php
@@ -343,7 +343,7 @@ class CustomAlerts extends PluginBase {
     /**
      * Check if death messages are custom
      * 
-     * @param EntityDeathEvent $cause
+     * @param EntityDamageEvent $cause
      *
      * @return bool
      */
